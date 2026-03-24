270d26246a5962421447508afcd072dae0dd62a4
zhangxujing <zhangxujing@hzlingyan.cn>
2026-03-24 21:31:55 +0800

    add:登录协议跳转

--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -192,20 +192,16 @@
         <div class="footer-agreement">
           <div class="agreement">
             {{ t('LoginPopUpPage.agreement') }}
-            <a href="#">{{ t('LoginPopUpPage.userAgreement') }}</a>
+            <a href="#" @click.prevent="goAgreement('USER_AGREEMENT')">{{ t('LoginPopUpPage.userAgreement') }}</a>
             {{ t('LoginPopUpPage.and') }}
-            <a href="#">{{ t('LoginPopUpPage.userPolicy') }}</a>
+            <a href="#" @click.prevent="goAgreement('PRIVACY_POLICY')">{{ t('LoginPopUpPage.userPolicy') }}</a>
           </div>
         </div>
       </div>
     </div>
     <Transition name="modal">
-      <ResetPassword
-          v-if="dialogs.isVisible"
-          :open-id="openId"
-          :mode="currentMode"
-          :confirmedInviteCode="confirmedInviteCode"
-          @close="dialogs.isVisible = false" />
+      <ResetPassword v-if="dialogs.isVisible" :open-id="openId" :mode="currentMode"
+        :confirmedInviteCode="confirmedInviteCode" @close="dialogs.isVisible = false" />
     </Transition>
     <InvitationCode v-if="dialogs.invitation" @update:visible="dialogs.invitation = $event"
       @confirm="handleInviteConfirm" />
@@ -215,6 +211,7 @@
 <script setup lang="ts">
 import { ref, reactive, watch, onUnmounted, onBeforeUnmount } from 'vue' // 补全了 onBeforeUnmount
 import { useI18n } from 'vue-i18n'
+import { useRouter } from 'vue-router'
 import { ElMessage } from 'element-plus'
 import iconEyesOpen from '@/assets/images/login_popup/eyes.png'
 import iconEyeClose from '@/assets/images/login_popup/eye_close.png'
@@ -228,6 +225,7 @@ import InvitationCode from '@/components/InvitationCode.vue'
 // ==========================================
 const userStore = useUserStore()
 const { t } = useI18n()
+const router = useRouter()
 const emit = defineEmits(['close'])
 import { images } from '@/assets'
 const accountType = ref<'personal' | 'team'>('personal') // 账号类型：个人 / 团队
@@ -245,6 +243,14 @@ const openId = ref('0') // 这个变脸给ResetPassword页面的绑定手机号�
 // 统一关闭弹窗的方法
 const handleClose = () => {
   emit('close')
+}
+
+const goAgreement = (type: 'USER_AGREEMENT' | 'PRIVACY_POLICY') => {
+  handleClose()
+  router.push({
+    path: '/agreement',
+    query: { type },
+  })
 }
 
 // ==========================================
@@ -316,7 +322,7 @@ const startPolling = () => {
             openId.value = res.data.openId
 
             ElMessage.success('扫码登录成功');
-          } else{
+          } else {
             userStore.setToken(res.data.accessToken);
             const aa = await userStore.getUserInfo() // 触发获用户信息接口
             console.log(aa)
